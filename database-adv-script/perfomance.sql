6a365153d7ad2ddf724e92c9f5881334309a75c7
Mikelesnr <mwanza.n.m@gmail.com>
2024-12-01 22:51:35 +0200

    Performance analysis

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -11,6 +11,28 @@ SELECT
     pay.amount
 FROM 
     Booking b
+WHERE
+    u.first_name = "John" AND u.last_name = "Doe"
+JOIN 
+    User u ON b.user_id = u.user_id
+JOIN 
+    Property p ON b.property_id = p.property_id
+JOIN 
+    Payment pay ON b.booking_id = pay.booking_id;
+
+EXPLAIN SELECT 
+    b.booking_id,
+    b.start_date,
+    u.user_id,
+    u.first_name,
+    p.property_id,
+    p.name,
+    pay.payment_id,
+    pay.amount
+FROM 
+    Booking b
+WHERE
+    u.first_name = "John" AND u.last_name = "Doe"
 JOIN 
     User u ON b.user_id = u.user_id
 JOIN 
